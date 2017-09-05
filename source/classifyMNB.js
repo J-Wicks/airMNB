3acde41b656729a65c2dd84c18bf615bd88a192d
Jeremy Wicks <artfullu@gmail.com>
2017-09-05 14:12:07 -0400

    made some tweaks to mnb classifier

--- source/classifyMNB.js
+++ source/classifyMNB.js
@@ -29,9 +29,16 @@ const fitTransformed = CountVectorizer().transform(fittedData);
 // console.log(fitTransformed);
 const tfidfTransformed = tfidfFitTransform(fitTransformed);
 const labels = fitTransformed.labels;
+const testString = `“The question,” old Castel cut in almost rudely, “is to know whether it's a plague or not.”
 
-console.log(classifyMNB().fit(tfidfTransformed, labels).predict('does this work'));
+Two or three of the doctors present protested. The others seemed to hesitate. The Prefect gave a start and hurriedly glanced toward the door to make sure it had prevented this outrageous remark from being overheard in the corridor. Richard said that in his opinion the great thing was not to take an alarmist view. All that could be said at present was that we had to deal with a special type of fever, with inguinal complications; in medical science, as in daily life, it was unwise to jump to conclusions.
 
+Rieux, who had said nothing so far, was asked for his opinion. “We are dealing,” he said, “with a fever of typhoidal nature, accompanied by vomiting and buboes. I have incised these buboes and had the pus analyzed; our laboratory analyst believes he has identified the plague bacillus. But I am bound to add that there are specific modifications that don't quite tally with the classical description of the plague bacillus.”
+`;
+
+console.log(classifyMNB().fit(tfidfTransformed, labels).predict(testString));
+
+console.log(classifyMNB().fit(tfidfTransformed, labels).predictProbability(testString));
 // tfidfFitTransform returns a matrix of term frequency inverse document frequency weights
 // in same order as cv().transform
 
--- source/trainMNB.js
+++ source/trainMNB.js
@@ -1,27 +1,10 @@
 const utils = require('./utils');
 const _ = require('lodash');
 
-const MultinomialNB = (options) => {
+// dry this out
+
+const MultinomialNB = function(options) {
   const fit = (model, words) => ({
-    predict: (doc) => {
-      const $doc = doc.toLowerCase();
-      const docTokens = Object.keys(utils.tokenize($doc));
-      const docVocab = _.intersection(docTokens, words);
-      const probabilities = Array(model.data[0].length).fill(0);
-      const tokenScores = utils.getTokenCounts(model.data);
-      // create an array with as many elements as there are in model[0]
-      // for every word in docVocab, find the probability from model
-      // add that probability to the value in the corresponding position in the array
-      docVocab.forEach((term) => {
-        const row = words.indexOf(term);
-        model.data[row].forEach((probab, idx) => {
-          probabilities[idx] += probab;
-        });
-      });
-      const scores = probabilities.map((sumScores, idx) => sumScores / tokenScores[idx]);
-      const maxIndex = scores.indexOf(Math.max(...scores));
-      return model.classes[maxIndex];
-    },
     predictProbability: (doc) => {
       const $doc = doc.toLowerCase();
       const docTokens = Object.keys(utils.tokenize($doc));
@@ -40,6 +23,26 @@ const MultinomialNB = (options) => {
       return probabilities.map((sumScores, idx) => {
         return sumScores / tokenScores[idx];
       });
+    },
+    predict: (doc) => {
+      const $doc = doc.toLowerCase().replace(/[^a-zA-Z ]/g, '');
+      const docTokens = Object.keys(utils.tokenize($doc));
+      console.log(docTokens);
+      const docVocab = _.intersection(docTokens, words);
+      const probabilities = Array(model.data[0].length).fill(0);
+      const tokenScores = utils.getTokenCounts(model.data);
+      // create an array with as many elements as there are in model[0]
+      // for every word in docVocab, find the probability from model
+      // add that probability to the value in the corresponding position in the array
+      docVocab.forEach((term) => {
+        const row = words.indexOf(term);
+        model.data[row].forEach((probab, idx) => {
+          probabilities[idx] += probab;
+        });
+      });
+      const scores = probabilities.map((sumScores, idx) => sumScores / tokenScores[idx]);
+      const maxIndex = scores.indexOf(Math.max(...scores));
+      return model.classes[maxIndex];
     },
   });
   return {fit};
